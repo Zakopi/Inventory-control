0064fe4672753b9ac759d92203321810fbd27395
Zakopi <mi7188hiza@g.kumamoto-nct.ac.jp>
2020-01-07 02:16:15 +0900

    メイン画面を作った

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.入荷 = new System.Windows.Forms.Button();
+            this.出荷 = new System.Windows.Forms.Button();
+            this.登録 = new System.Windows.Forms.Button();
+            this.バーコード = new System.Windows.Forms.Button();
+            this.一覧 = new System.Windows.Forms.Button();
+            this.グラフ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // 入荷
+            // 
+            this.入荷.Location = new System.Drawing.Point(102, 25);
+            this.入荷.Name = "入荷";
+            this.入荷.Size = new System.Drawing.Size(226, 86);
+            this.入荷.TabIndex = 0;
+            this.入荷.Tag = "";
+            this.入荷.Text = "入荷";
+            this.入荷.UseVisualStyleBackColor = true;
+            this.入荷.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 出荷
+            // 
+            this.出荷.Location = new System.Drawing.Point(102, 136);
+            this.出荷.Name = "出荷";
+            this.出荷.Size = new System.Drawing.Size(226, 86);
+            this.出荷.TabIndex = 0;
+            this.出荷.Text = "出荷";
+            this.出荷.UseVisualStyleBackColor = true;
+            this.出荷.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // 登録
+            // 
+            this.登録.Location = new System.Drawing.Point(102, 245);
+            this.登録.Name = "登録";
+            this.登録.Size = new System.Drawing.Size(226, 86);
+            this.登録.TabIndex = 0;
+            this.登録.Text = "登録";
+            this.登録.UseVisualStyleBackColor = true;
+            this.登録.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // バーコード
+            // 
+            this.バーコード.Location = new System.Drawing.Point(424, 25);
+            this.バーコード.Name = "バーコード";
+            this.バーコード.Size = new System.Drawing.Size(226, 86);
+            this.バーコード.TabIndex = 0;
+            this.バーコード.Text = "バーコード";
+            this.バーコード.UseVisualStyleBackColor = true;
+            this.バーコード.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 一覧
+            // 
+            this.一覧.Location = new System.Drawing.Point(424, 136);
+            this.一覧.Name = "一覧";
+            this.一覧.Size = new System.Drawing.Size(226, 86);
+            this.一覧.TabIndex = 0;
+            this.一覧.Text = "一覧";
+            this.一覧.UseVisualStyleBackColor = true;
+            this.一覧.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // グラフ
+            // 
+            this.グラフ.Location = new System.Drawing.Point(424, 245);
+            this.グラフ.Name = "グラフ";
+            this.グラフ.Size = new System.Drawing.Size(226, 86);
+            this.グラフ.TabIndex = 0;
+            this.グラフ.Text = "グラフ";
+            this.グラフ.UseVisualStyleBackColor = true;
+            this.グラフ.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.グラフ);
+            this.Controls.Add(this.一覧);
+            this.Controls.Add(this.登録);
+            this.Controls.Add(this.バーコード);
+            this.Controls.Add(this.出荷);
+            this.Controls.Add(this.入荷);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button 出荷;
+        private System.Windows.Forms.Button バーコード;
+        private System.Windows.Forms.Button 一覧;
+        private System.Windows.Forms.Button グラフ;
+        public System.Windows.Forms.Button 入荷;
+        public System.Windows.Forms.Button 登録;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,37 @@ namespace Inventory_control
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Tom\source\repos\Inventory control\test.txt";
+
+            // ファイルを書き込みモード開く
+            StreamWriter writer = new StreamWriter(filePath, true, Encoding.GetEncoding("Shift_JIS"));
+
+            string str = Console.ReadLine();
+
+            // ファイルに書き込む
+            writer.WriteLine(str);
+            // ファイルを閉じる
+            writer.Close();
+
+            StreamReader reader = new StreamReader(filePath, Encoding.GetEncoding("Shift_JIS"));
+
+            while (reader.Peek() >= 0)
+            {
+                string[] cols = reader.ReadLine().Split(',');
+                for (int n = 0; n < cols.Length; n++)
+                    Console.Write(cols[n] + "\t");
+                Console.WriteLine("");
+            }
+            reader.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
